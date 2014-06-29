ebea3b817d53394a396ff1603ace457f834695e9
Georges Dubus <georges.dubus@compiletoi.net>
2014-06-29 19:06:42 +0200

    Added a nice demo for the main

--- src/oxydation.rs
+++ src/oxydation.rs
@@ -1,7 +1,23 @@
 use board::Board;
+use std::rand::{task_rng, Rng};
+use std::io::timer::sleep;
 mod board;
 
 fn main() {
+    demo();
+}
+
+fn demo() {
     let mut b = Board::new(10, 10);
-    println!("{}", b);
+    let mut rng = task_rng();
+    loop {
+        b.set(rng.gen_range(0u, 10u),
+              rng.gen_range(0u, 10u),
+              rng.gen_range(1u, 10u));
+        println!("{}", b);
+        sleep(500);
+        b.apply_gravity();
+        println!("{}", b);
+        sleep(500);
+    }
 }
